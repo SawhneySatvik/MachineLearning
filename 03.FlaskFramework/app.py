2b5a9ac426f86f822abfc866c1352aaf3d222f93
Satvik Sawhney <satvik.sawhney2005@gmail.com>
2025-01-13 10:21:27 +0530

    Simple flask app:

--- 03.FlaskFramework/app.py
+++ 03.FlaskFramework/app.py
@@ -1,1 +1,19 @@
-import flask
+from flask import Flask
+from flask import render_template
+
+#WSGI Application
+app = Flask(__name__)
+'''
+It creates an instance of a flask class, whcih will be your Web server gateway interface
+'''
+
+@app.route('/')
+def home():
+    return 'Welcome to flask application'
+
+@app.route('/render_template')
+def renderer():
+    return 'Hi'
+
+if __name__ == '__main__':
+    app.run(debug=True)
